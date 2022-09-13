9dec5dc6731c27fe9ab217038ccd66813c7043f8
enedaveed <enedave2001@gmail.com>
2022-09-13 07:15:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "putchar.h"
+#include "_putchar.h"
 
 /**
  * main - Entry point
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');

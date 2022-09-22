33aefbb4373a99f954df4c035dc840bf481ae738
enedaveed <enedave2001@gmail.com>
2022-09-22 06:07:25 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,24 @@
 #include "main.h"
 /**
- * _indexOf - returns boolean if special  character
- * @a: character to return
- * Return: true or false
- */
-
-int _indexOf(char a)
-{
-	int i;
-	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',')', '{', '}'};
-
-	for (i = 0; i < 13; i++)
-	{
-		if (capArr[i] == a)
-			return (1);
-	}
-	return (0);
-}
-/**
- * cap_string - capitalizes the string
- * @s: string
- * Return: the string capitalized
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
  */
 
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 'a' + 'A';
+	i++;
+
+	while (s[i] != '\0') /* iterate through string */
 	{
-		if (_indexOf(s[i]))
-			continue;
-		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
-			s[i] = s[i] - 32;
-
+		/* if lowercase and prior char is separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n')) s[i] = s[i] - 'a' + 'A';
+		i++;
 	}
 
 	return (s);

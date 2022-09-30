6ce3b389bf19bc6ff8ee4c62654b4028bef6cd00
enedaveed <enedave2001@gmail.com>
2022-09-30 14:14:52 -0700

    static library

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to append by src
+ * @src: string to append to dest
+ *
+ * Return: address of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+
+	return (dest);
+}
